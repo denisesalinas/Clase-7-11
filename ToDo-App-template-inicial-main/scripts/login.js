330e1f129e1f0d9d2b2aba5791ebe8ce80953755
Denise Salinas <denisesalinas3@gmail.com>
2024-11-07 21:07:57 -0300

    Login y Signup en clase 7-11

--- ToDo-App-template-inicial-main/scripts/login.js
+++ ToDo-App-template-inicial-main/scripts/login.js
@@ -1,19 +1,32 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
-    
-
+   const form= document.forms[0]
+   const email= document.querySelector('#inputEmail')
+   const password= document.querySelector('#inputPassword')
+    const url= 'https://unlpam-todo-api.vercel.app'
 
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-       
-        
+      //
 
+        //Información del formulario
+        const payload= {
+        email: email.value,
+        password:password.value
+       }
 
-
+       //Configuración del POST
+       const settings={
+        method:'POST',
+        body: JSON.stringify(payload),
+        headers:{
+            'Content-Type' : 'application/json'
+        }
+       }
+        realizarLogin(settings)
     });
 
 
@@ -21,7 +34,33 @@ window.addEventListener('load', function () {
     /*                     FUNCIÓN 2: Realizar el login [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarLogin(settings) {
-       
+       console.log(settings)
+       fetch(`${url}/users/login`, settings)
+       //Escuchamos la respuesta
+       .then (response =>{
+        if(response.ok!=true){
+           if (response.status ==404){
+            alert('El usuario no existe')
+           } 
+           if( response.status == 400){
+            alert('La contraseña ingresada no es válida')
+           }
+           if( response.status == 500){
+            alert('Error en el servidor')
+           }
+        }
+        return response.json()
+       })
+       //Datos en JSON
+       .then(data=>{
+        console.log(data.jwt)
+        //Preguntar si existe jwt
+        if(data.jwt) {
+            //Se guarda en el local
+            localStorage.setItem('jwt',JSON.stringify(data.jwt))
+        }
+        location.replace('./mis-tareas.html')
+       })
 
 
 
--- ToDo-App-template-inicial-main/scripts/signup.js
+++ ToDo-App-template-inicial-main/scripts/signup.js
@@ -1,31 +1,57 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
-
-
-    
+    const form= document.forms[0]
+    const firstName= document.querySelector('#inputNombre')
+    const lastName= document.querySelector('#inputApellido')
+    const email= document.querySelector('#inputEmail')
+    const password= document.querySelector('#inputPassword')
+    const passwordRepetida= document.querySelector('#inputPasswordRepetida')
+    const url= 'https://unlpam-todo-api.vercel.app/users'    
 
     /* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
-      
+      event.preventDefault()
+      const payload ={
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,
+            password:password.value        
+      }
 
-
-
-
+      const settings = {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+             'Content-Type': 'application/json'
+        }
+      }
+      realizarRegister(settings)
     });
 
     /* -------------------------------------------------------------------------- */
     /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
     /* -------------------------------------------------------------------------- */
     function realizarRegister(settings) {
+        fetch (`${url}/signup`,settings)
+        .then (response =>{
+            if(response.ok!= true){
+                if( response.status == 400){
+                    alert('El usuario ya se encuentra registrado / Alguno de los datos requeridos estan incompletos')
+                   }
+                   if( response.status == 500){
+                    alert('Error en el servidor')
+                   }
+                }
+            return response.JSON()
+            })
         
-
-
-
-
-    };
-
-
+        .then(data =>{
+            if(data.jwt){
+                localStorage.setItem('jwt', JSON.stringify(data.jwt))
+            }
+            location.replace('./mis-tareas.html')
+        })
+    }
 });
